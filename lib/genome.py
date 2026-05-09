17f1e175472e9cdffc0857472330e3b186361903
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-05-08 17:08:32 -0700

    Add comments to genome.py

--- lib/genome.py
+++ lib/genome.py
@@ -15,8 +15,11 @@ g = c = t = a = 0
 N = n = 0
 total = 0
 
+# open the gz file
 with gzip.open(args.input, 'rt') as f:
+	# iterate over each line
 	for line in f:
+		# check lines only if they are not header lines
 		if not line.startswith('>'):
 			G += line.count("G")
 			C += line.count("C")
@@ -30,8 +33,7 @@ with gzip.open(args.input, 'rt') as f:
 			n += line.count("n")
 			total += len(line) - 1
 
-
-
+# print total counts per base
 print("G: ", G+g)
 print("C: ", C+c)
 print("T: ", T+t)
@@ -40,6 +42,7 @@ print("N: ", N+n)
 
 print()
 
+# print various totals
 print("Total bases: ", total)
 print("Total unambiguous bases: ", total-N-n)
 print("Total unmasked bases: ", G+C+T+A)
@@ -47,6 +50,7 @@ print("Total masked bases: ", g+c+t+a)
 
 print()
 
+# print GC content
 print("Total GC content: ", f"{(G+g+C+c)/(total-N-n)*100:.5}%")
 print("Unmasked GC content: ", f"{(G+C)/(G+C+T+A)*100:.5}%")
 
