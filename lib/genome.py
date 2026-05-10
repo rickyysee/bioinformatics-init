28cf566a524c586c0280ff4c1ba96bb87d81a3f9
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-05-10 13:03:05 -0700

    Refactor if statements to be less redundant

--- lib/genome.py
+++ lib/genome.py
@@ -4,27 +4,23 @@ import argparse
 import sys
 import gzip
 from collections import defaultdict
-import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 
+# Add arguments that user can
 parser.add_argument('input', help='Input file to work with.')
 parser.add_argument('-c', '--chr', action='store_true', help='Gather statistics by chromosomes.')
 args = parser.parse_args()
 
-# initialize all variables as zero
+# Initialize all variables as zero
 G = C = T = A = g = c = t = a = N = n = total = 0
 
-results = {} # { header: {G, C, A, T, N, total}}
-current_header = None
-
 if args.chr == False:
-# open the gz file
+# Open the gz file
 	with gzip.open(args.input, 'rt') as f:
-		# iterate over each line
+		# Iterate over each line
 		for line in f:
-			line = line.rstrip('\n')
-			# check lines only if they are not header lines
+			# Check lines only if they are not header lines
 			if line.startswith('>'):
 				continue
 			G += line.count("G")
@@ -37,8 +33,29 @@ if args.chr == False:
 			a += line.count("a")
 			N += line.count("N")
 			n += line.count("n")
-			total += len(line)
+			total += len(line) - 1
+	# Print total counts per base
+	print("G: ", G+g)
+	print("C: ", C+c)
+	print("T: ", T+t)
+	print("A: ", A+a)
+	print("N: ", N+n)
+	print()
+	# Print various totals
+	print("Total bases: ", total)
+	print("Total unambiguous bases: ", total-N-n)
+	print("Total unmasked bases: ", G+C+T+A)
+	print("Total masked bases: ", g+c+t+a)
+	print()
+	# Print GC content
+	print("Total GC content: ", f"{(G+g+C+c)/(total-N-n)*100:.5}%")
+	print("Unmasked GC content: ", f"{(G+C)/(G+C+T+A)*100:.5}%")
+	print()
+
 elif args.chr == True:
+	# Start a dictionary to store results
+	results = {} # { header: {G, C, A, T, N, total}}
+	current_header = None
 	with gzip.open(args.input, 'rt') as f:
 		for line in f:
 			line = line.rstrip('\n')
@@ -63,27 +80,3 @@ elif args.chr == True:
 		if total > 0:
 			gc = (counts['G'] + counts['g'] + counts['C'] + counts['c']) / total * 100
 			print(f"{header}  GC%: {gc:.2f}%  total_bases: {total}")
-
-if args.chr == False:
-	# print total counts per base
-	print("G: ", G+g)
-	print("C: ", C+c)
-	print("T: ", T+t)
-	print("A: ", A+a)
-	print("N: ", N+n)
-
-	print()
-
-	# print various totals
-	print("Total bases: ", total)
-	print("Total unambiguous bases: ", total-N-n)
-	print("Total unmasked bases: ", G+C+T+A)
-	print("Total masked bases: ", g+c+t+a)
-
-	print()
-
-	# print GC content
-	print("Total GC content: ", f"{(G+g+C+c)/(total-N-n)*100:.5}%")
-	print("Unmasked GC content: ", f"{(G+C)/(G+C+T+A)*100:.5}%")
-
-	print()
