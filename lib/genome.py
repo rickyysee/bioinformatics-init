5325842c92f74cda1b645d92106fed7c37db839d
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-09-04 00:47:23 -0700

    change to single quotes

--- lib/genome.py
+++ lib/genome.py
@@ -29,21 +29,21 @@ if byDef == False:
 	total = G+g+C+c+T+t+A+a+N+n
 
 	# print total counts per base
-	print("G: ", G+g)
-	print("C: ", C+c)
-	print("T: ", T+t)
-	print("A: ", A+a)
-	print("N: ", N+n)
+	print('G: ', G+g)
+	print('C: ', C+c)
+	print('T: ', T+t)
+	print('A: ', A+a)
+	print('N: ', N+n)
 	print()
 	# print various totals
-	print("Total bases: ", total)
-	print("Total unambiguous bases: ", total-N-n)
-	print("Total unmasked bases: ", G+C+T+A)
-	print("Total masked bases: ", g+c+t+a)
+	print('Total bases: ', total)
+	print('Total unambiguous bases: ', total-N-n)
+	print('Total unmasked bases: ', G+C+T+A)
+	print('Total masked bases: ', g+c+t+a)
 	print()
 	# print GC content
-	print("Total GC content: ", f"{(G+g+C+c)/(total-N-n)*100:.5}%")
-	print("Unmasked GC content: ", f"{(G+C)/(G+C+T+A)*100:.5}%")
+	print('Total GC content: ', f'{(G+g+C+c)/(total-N-n)*100:.5}%')
+	print('Unmasked GC content: ', f'{(G+C)/(G+C+T+A)*100:.5}%')
 	print()
 
 
@@ -65,7 +65,7 @@ elif byDef == True:
 	
 	
 	
-	"""
+	'''
 	# start a dictionary to store results
 	results = {} # { header: {G, C, A, T, N, total}}
 	current_header = None
@@ -91,5 +91,5 @@ elif byDef == True:
 		total = counts['total']
 		if total > 0:
 			gc = (counts['G'] + counts['g'] + counts['C'] + counts['c']) / total * 100
-			print(f"{header}  GC%: {gc:.2f}%  total_bases: {total}")
-	"""
+			print(f'{header}  GC%: {gc:.2f}%  total_bases: {total}')
+	'''
