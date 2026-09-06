bc4caf22545174b27fbb1fb7ab635dfc9758b329
rcantua <156491520+rickyysee@users.noreply.github.com>
2026-09-06 00:26:07 -0700

    add TSV output

--- lib/genome.py
+++ lib/genome.py
@@ -8,22 +8,74 @@ parser = argparse.ArgumentParser(description='gather counts of a fasta file')
 # handle arguments and assign to variables
 parser.add_argument('input', help='input file to work with')
 parser.add_argument('-d', '--defline', action='store_true', help='gather statistics by defline')
+parser.add_argument('-t', '--tsv', action='store_true', help='output as TSV format')
+parser.add_argument('-a', '--all', action='store_true', help='distinguish masked bases if outputting in TSV format')
 args = parser.parse_args()
 
-file = args.input
+fasta = args.input
 byDef = args.defline
+outTSV = args.tsv
+outAll = args.all
 
-if byDef == False:
-	# count bases with dict
-	counts = {}
-	for defline, seq in sequence.read_fasta(file):
-		for nt in seq:
-			if nt not in counts: counts[nt] = 0
-			counts[nt] += 1
+# print a header line for TSV
+if byDef:
+	if outTSV and outAll:
+		print(f'def\tG\tg\tC\tc\tT\tt\tA\ta\tN\tn\ttotal')
+	if outTSV and not outAll:
+		print(f'def\tG\tC\tT\tA\tN\ttotal')
 
-	# initialize variables based on a set alphabet
+# create a dictionary to store all results
+results = {} # {defline: {G, g, ... N, n, total}}
+this_defline = None
+
+# read in the fasta
+for defline, seq in sequence.read_fasta(fasta):
+
+	# create a dictionary for the current defline
+	this_defline = defline
+	if this_defline not in results: results[this_defline] = {}
+	counts = results[this_defline]
+
+	# count the bases in this defline's sequence
+	for nt in seq:
+		if nt not in counts: counts[nt] = 0
+		counts[nt] += 1
+	
+	### dictates whether to print out by defline ###
+	if byDef == False: continue
+
+	# store dictionary counts in variables for easier calling
 	bases = ['G', 'g', 'C', 'c', 'T', 't', 'A', 'a', 'N', 'n']
 	G, g, C, c, T, t, A, a, N, n = (counts.get(b, 0) for b in bases)
+	total = G + g + C + c + T + t + A + a + N + n
+
+	# do not print deflines with no sequence
+	if total == 0: continue
+	gc = (G + C + g + c) / total
+
+	# print out counts depending on args
+	if not outTSV:
+		print(f'>{defline}')
+		print(f'G:{G+g}\tC:{C+c}\tT:{T+t}\tA:{A+a}\tN:{N+n}')
+		print(f'GC:{gc*100:.2f}%\ttotal:{total}')
+		
+	elif outAll:
+		print(f'\'{defline}\'\t{G}\t{g}\t{C}\t{c}\t{T}\t{t}\t{A}\t{a}\t{N}\t{n}\t{total}')
+
+	elif not outAll:
+		print(f'\'{defline}\'\t{G+g}\t{C+c}\t{T+t}\t{A+a}\t{N+n}\t{total}')
+
+if not byDef:
+
+	# create a new dict with each base summed up
+	totals = {}
+	for defline, counts in results.items():
+		for nt, count in counts.items():
+			totals[nt] = totals.get(nt, 0) + count
+
+	# store dictionary counts in variables for easier calling
+	bases = ['G', 'g', 'C', 'c', 'T', 't', 'A', 'a', 'N', 'n']
+	G, g, C, c, T, t, A, a, N, n = (totals.get(b, 0) for b in bases)
 	total = G + g + C + c + T + t + A + a + N + n
 
 	# print total counts per base
@@ -42,36 +94,3 @@ if byDef == False:
 	# print GC content
 	print(f'Total GC content: {(G+g+C+c)/(total-N-n)*100:.5}%')
 	print(f'Unmasked GC content: {(G+C)/(G+C+T+A)*100:.5}%')
-
-
-elif byDef == True:
-	# create a dictionary to store all results
-	results = {} # {defline: {G, g, ... N, n, total}}
-	this_defline = None
-
-	# read in the fasta
-	for defline, seq in sequence.read_fasta(file):
-
-		# create a dictionary for the current defline
-		this_defline = defline
-		if this_defline not in results: results[this_defline] = {}
-		counts = results[this_defline]
-
-		# count the bases in this defline's sequence
-		for nt in seq:
-			if nt not in counts: counts[nt] = 0
-			counts[nt] += 1
-
-		# store dictionary counts in variables for easier calling
-		bases = ['G', 'g', 'C', 'c', 'T', 't', 'A', 'a', 'N', 'n']
-		G, g, C, c, T, t, A, a, N, n = (counts.get(b, 0) for b in bases)
-		total = G + g + C + c + T + t + A + a + N + n
-
-		# do not print deflines with no sequence
-		if total == 0: continue
-
-		gc = (G + C + g + c) / total
-
-		print(f'>{defline}')
-		print(f'G:{G+g}\tC:{C+c}\tT:{T+t}\tA:{A+a}\tN:{N+n}')
-		print(f'GC:{gc*100:.2f}%\ttotal:{total}')
